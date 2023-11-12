85c44bec9cc3ef9da77a71e80e06118600c0b29f
chen <23054115+cwkang1998@users.noreply.github.com>
2023-11-12 15:08:23 +0800

    feat: impl cairo utils

--- prover_verifier_backend/src/cairo_utils/mod.rs
+++ prover_verifier_backend/src/cairo_utils/mod.rs
@@ -1,13 +1,68 @@
-use std::{process::Command, str::from_utf8};
+use std::{
+    future::Future,
+    path::{Path, PathBuf},
+    process::Command,
+    str::from_utf8,
+};
 
+use async_recursion::async_recursion;
 use regex::Regex;
+use rocket::futures::TryFutureExt;
+use tokio::{fs::{self, File}, io::{self, AsyncReadExt, AsyncWriteExt}};
 
-pub async fn prepare_inference_environment(model_path: &str) {
-    // TODO
+#[async_recursion]
+async fn copy_recursively(source: PathBuf, destination: PathBuf) -> io::Result<()> {
+    fs::create_dir_all(&destination).await?;
+    let mut dir = fs::read_dir(&source).await?;
+    while let Some(entry) = dir.next_entry().await? {
+        let filetype = entry.file_type().await?;
+        if filetype.is_dir() {
+            copy_recursively(entry.path(), destination.clone().join(entry.file_name())).await?;
+        } else {
+            fs::copy(entry.path(), destination.clone().join(entry.file_name())).await?;
+        }
+    }
+    Ok(())
+}
+pub async fn prepare_inference_environment(proof_id: String) -> Result<(), String> {
+    copy_recursively(
+        PathBuf::from("tic_tac_toe_orion"),
+        PathBuf::from(format!("inference_result/{}/orion", proof_id)),
+    )
+    .map_err(|err| err.to_string())
+    .await?;
+    Ok(())
 }
 
-pub async fn convert_input_to_cairo(input_str: &str, write_path: &str) {
-    
+pub async fn convert_ttt_input_to_cairo(input_str: &str, write_path: &str) -> Result<(), String> {
+    // Hard coded to tic tac toe for now
+    // TODO: MAKE IT GENERAL
+    let rows: Vec<&str> = input_str.split(';').collect();
+
+    fn num_str_to_str(num_str: &str) -> String {
+        if num_str == "0" {
+            return "zero".to_owned()
+        }
+        if num_str == "1"  {
+            return "one".to_owned()
+        }
+        "two".to_owned()
+    }
+
+    let row1: Vec<String> = rows[0].split(',').map(num_str_to_str).collect();
+    let row2: Vec<String> = rows[1].split(',').map(num_str_to_str).collect();
+    let row3: Vec<String> = rows[2].split(',').map(num_str_to_str).collect();
+
+    // Read from file now
+    let mut file = File::open(&write_path).await.map_err(|err| err.to_string())?;
+    let mut contents = String::new();
+	file.read_to_string(&mut contents).await.map_err(|err| err.to_string())?;
+
+    row1.iter().for_each(|entry| contents = contents.replacen("{%%}", entry, 1));
+    row2.iter().for_each(|entry| contents = contents.replacen("{%%}", entry, 1));
+    row3.iter().for_each(|entry| contents = contents.replacen("{%%}", entry, 1));
+    fs::write(&write_path, contents).await.map_err(|err| err.to_string())?;
+    Ok(())
 }
 
 pub async fn run_inference(program_path: &str) -> Result<Vec<String>, String> {
--- prover_verifier_backend/src/testing.rs
+++ prover_verifier_backend/src/testing.rs
@@ -2,7 +2,10 @@ mod cairo_utils;
 
 #[tokio::main]
 pub async fn main() -> Result<(), String> {
-    let outputs = cairo_utils::run_inference("tic_tac_toe_orion").await?;
+    cairo_utils::prepare_inference_environment("abc1".to_owned()).await?;
+    cairo_utils::convert_ttt_input_to_cairo("1,1,2;2,2,2;0,0,2", "inference_result/abc1/orion/src/test.cairo").await?;
+    cairo_utils::convert_ttt_input_to_cairo("1,1,2;2,2,2;0,0,2", "inference_result/abc1/orion/src/inference.cairo").await?;
+    let outputs = cairo_utils::run_inference("inference_result/abc1/orion").await?;
     println!("{:?}", outputs);
     Ok(())    
 }
